02f01bcae9d3f5be152bb914ccac138909bc81d7
JuanSMartinez <js.martinez777@uniandes.edu.co>
2017-05-09 16:30:45 -0500

    added haptic object script

---
+++ HapticGlove/Assets/Scripts/HapticObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionStay(Collision collision){
+		Debug.Log ("Collided with" + collision.collider.name);
+	}
+}
